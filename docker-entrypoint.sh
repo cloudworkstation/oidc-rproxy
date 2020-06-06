11cf5b1ab382cff1e091f30ad775b205209ffb46
Richard Kendall <richard.kendall@gmail.com>
2020-06-06 19:27:53 +1000

    switched to ubuntu from centos

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${SCHEME} ${PORT} ${UPSTREAM} ${OIDC_CRYPTO_PASSPRHASE} ${DOMAIN} ${OIDC_CLIENT_SECRET} ${OIDC_CLIENT_ID} ${OIDC_PROVIDER_JWKS_URI} ${OIDC_PROVIDER_METADATA_URL}' < /etc/httpd/conf.d/auth.conf.template > /etc/httpd/conf.d/auth.conf
+envsubst '${SCHEME} ${PORT} ${UPSTREAM} ${OIDC_CRYPTO_PASSPRHASE} ${DOMAIN} ${OIDC_CLIENT_SECRET} ${OIDC_CLIENT_ID} ${OIDC_PROVIDER_JWKS_URI} ${OIDC_PROVIDER_METADATA_URL}' < /apache.conf > /etc/apache2/sites-available/000-default.conf
 
 exec "$@"
